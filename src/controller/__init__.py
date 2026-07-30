5957e247969e99b393c7a527c10224f60c75fcf6
DuyThong <duythongtrannguyen98@gmail.com>
2026-07-30 17:31:28 +0700

    remove error

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter
 from controller.auth import router as auth_router
 from controller.home import router as home_router
 from controller.webrtc import router as webrtc_router
-from controller.evaluate_controller import router as evaluate_router
 from controller.archive_controller import router as archive_router
 from controller.realtime_controller import router as realtime_router
 from controller.avatar_webrtc_controller import router as avatar_webrtc_router
@@ -15,7 +14,6 @@ router = APIRouter()
 router.include_router(auth_router)
 router.include_router(home_router)
 router.include_router(webrtc_router)
-router.include_router(evaluate_router)
 router.include_router(archive_router)
 router.include_router(realtime_router)
 router.include_router(avatar_webrtc_router)
